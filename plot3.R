74d133f01fb2ae1ab9a02d5a326d78ff9c0e0c0d
olaolu <olaolu@users.noreply.github.com>
2014-12-14 23:31:38 -0700

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -19,16 +19,17 @@ datasub$DateTime =strptime(datasub$DateTime, format="%Y-%m-%d %H:%M:%S")
 
 png(filename = "plot3.png", width = 480, height = 480, units = "px", bg = "white")
 
-plot(data_subset$Date_Time, data_subset$Sub_metering_1, xaxt=NULL, xlab = "", ylab = "Energy sub metering", type="n")
+plot(datasub$Date_Time, datasub$Sub_metering_1, xaxt=NULL, xlab = "", ylab = "Energy sub metering", type="n")
 ## Sets up the plot, but does not populate with any data
 
-lines(data_subset$Date_Time, data_subset$Sub_metering_1, col = "black", type = "S")
+lines(datasub$Date_Time, datasub$Sub_metering_1, col = "black", type = "S")
 
-lines(data_subset$Date_Time, data_subset$Sub_metering_2, col = "red", type = "S")
+lines(datasub$Date_Time, datasub$Sub_metering_2, col = "red", type = "S")
 
-lines(data_subset$Date_Time, data_subset$Sub_metering_3, col = "blue", type = "S")
+lines(datasub$Date_Time, datasub$Sub_metering_3, col = "blue", type = "S")
 
 
 legend("topright", lwd = c(1, 1, 1), lty = c(1, 1),  col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
 
 dev.off()
+
